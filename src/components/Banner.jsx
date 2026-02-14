6b81e8b3d98db6bfa9643eab39c49dcfdd0f2b5f
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-14 22:37:10 +0600

    responsive banner part

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,17 +2,26 @@ import { Link } from 'react-router-dom'
 import banImg from '../assets/image/bannerHome.png'
 
 const Banner = () => {
-
   return (
-    <>
-      <section className='w-full'>
-        <div className="">
-          <Link to='/shop'>
-            <img className='w-full object-cover lg:h-175' src={banImg} alt="banner" />
-          </Link>
-        </div>
-      </section>
-    </>
+    <section className='w-full overflow-hidden'>
+      
+      <Link to='/shop' className='block w-full'>
+        <img
+          src={banImg}
+          alt="banner"
+          className='
+            w-full 
+            object-cover 
+            h-[180px] 
+            sm:h-[260px] 
+            md:h-[380px] 
+            lg:h-[500px] 
+            xl:h-[700px]
+          '
+        />
+      </Link>
+
+    </section>
   )
 }
 
