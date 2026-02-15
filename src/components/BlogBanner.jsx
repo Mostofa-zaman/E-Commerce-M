686dfefb5a86bed4136c1f85ca53d81fa825de61
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-15 21:53:40 +0600

    make BlogBanner section responsive across all devices

--- src/components/BlogBanner.jsx
+++ src/components/BlogBanner.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 
 const BlogBanner = () => {
     return (
-        <>
-            <section className='lg:py-20 py-10 bg-[#F6F5FF]'>
-                <div className="lg:w-9/12 w-11/12 mx-auto">
-                    <h2 className='text-[#101750] font-bold text-[36px] font-josefin'>Blog Page</h2>
-                    <div className="flex items-center lg:mt-2 mt-1">
-                        <p>Home.pages.</p><p className='text-[#FB2E86]'>Blog</p>
-                    </div>
+        <section className='py-8 sm:py-10 lg:py-20 bg-[#F6F5FF]'>
+            <div className="max-w-[1320px] w-11/12 mx-auto">
+
+                <h2 className='text-[#101750] font-bold text-[24px] sm:text-[28px] md:text-[32px] lg:text-[36px] font-josefin'>
+                    Blog Page
+                </h2>
+
+                <div className="flex items-center mt-1 sm:mt-2 text-[14px] sm:text-[15px]">
+                    <p>Home . Pages .</p>
+                    <p className='text-[#FB2E86] ml-1'>Blog</p>
                 </div>
-            </section>
-        </>
+
+            </div>
+        </section>
     )
 }
 
