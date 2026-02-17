fa9cc6b0510478231aec8406fefa9307a1c04e48
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-17 23:46:44 +0600

    improve responsiveness and layout alignment for heading part

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,68 +1,65 @@
-import { MdOutlineMail } from "react-icons/md";
+import { MdOutlineMail, MdOutlineWifiCalling3, MdOutlineFavoriteBorder } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
-import { MdOutlineWifiCalling3 } from "react-icons/md";
-import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Heading = () => {
-    let cart = useSelector((state) => state.products.carItem);
+  const cart = useSelector((state) => state.products.carItem);
 
-    return (
-        <div className="py-2 bg-[#7E33E0]">
-            <div className="lg:w-9/12 sm:w-10/12 w-[95%] mx-auto">
+  return (
+    <header className="py-2 bg-[#7E33E0]">
+      <div className="w-[95%] sm:w-10/12 lg:w-9/12 mx-auto">
 
-                <div className="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0">
 
-                    {/* Left */}
-                    <div className="flex flex-wrap justify-center md:justify-start items-center 
-                                    gap-2 sm:gap-6 text-white text-[12px] sm:text-[14px] md:text-[16px] font-josefin">
+          {/* Left Section */}
+          <div className="flex flex-wrap justify-center md:justify-start items-center gap-2 sm:gap-6 text-white text-[12px] sm:text-[14px] md:text-[16px] font-josefin">
 
-                        <div className="flex items-center gap-2">
-                            <MdOutlineMail />
-                            <p className="break-all">mhhasanul@gmail.com</p>
-                        </div>
-
-                        <div className="flex items-center gap-2">
-                            <MdOutlineWifiCalling3 />
-                            <p>(12345)67890</p>
-                        </div>
-                    </div>
-
-                    {/* Right */}
-                    <div className="flex items-center gap-3 sm:gap-5 text-white 
-                                    text-[12px] sm:text-[14px] md:text-[16px] font-josefin">
-
-                        <div className="flex items-center gap-2 cursor-pointer">
-                            <p>Login</p>
-                            <FaRegUser />
-                        </div>
-
-                        <div className="flex items-center gap-2 cursor-pointer">
-                            <p>Wishlist</p>
-                            <MdOutlineFavoriteBorder />
-                        </div>
-
-                        <Link
-                            to={"/cart"}
-                            className="flex items-center gap-2 cursor-pointer relative"
-                        >
-                            {cart.length > 0 && (
-                                <span className="bg-[#FB2E86] text-white rounded-full 
-                                                 w-5 h-5 flex items-center justify-center 
-                                                 text-[10px] sm:text-xs absolute -top-2 -right-2">
-                                    {cart.length}
-                                </span>
-                            )}
-                            <RiShoppingCartLine size={20} />
-                        </Link>
-                    </div>
-
-                </div>
+            <div className="flex items-center gap-2">
+              <MdOutlineMail />
+              <p className="break-all">mhhasanul@gmail.com</p>
             </div>
+
+            <div className="flex items-center gap-2">
+              <MdOutlineWifiCalling3 />
+              <p>(12345)67890</p>
+            </div>
+
+          </div>
+
+          {/* Right Section */}
+          <div className="flex items-center gap-3 sm:gap-5 text-white text-[12px] sm:text-[14px] md:text-[16px] font-josefin">
+
+            <div className="flex items-center gap-2 cursor-pointer">
+              <p>Login</p>
+              <FaRegUser />
+            </div>
+
+            <div className="flex items-center gap-2 cursor-pointer">
+              <p>Wishlist</p>
+              <MdOutlineFavoriteBorder />
+            </div>
+
+            <Link
+              to="/cart"
+              className="flex items-center gap-2 cursor-pointer relative"
+            >
+              {cart.length > 0 && (
+                <span className="absolute -top-2 -right-2 w-5 h-5 flex items-center justify-center rounded-full bg-[#FB2E86] text-white text-[10px] sm:text-xs">
+                  {cart.length}
+                </span>
+              )}
+
+              <RiShoppingCartLine size={20} />
+            </Link>
+
+          </div>
+
         </div>
-    );
+      </div>
+    </header>
+  );
 };
 
 export default Heading;
