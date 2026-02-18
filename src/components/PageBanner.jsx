e240c61b67f5513d95ed8de7461ec3cc4dc05b10
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-18 23:30:03 +0600

    enhance PageBanner responsiveness for all screen sizes

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
 const PageBanner = () => {
-    return (
-        <>
-            <section className='lg:py-20 py-10 bg-[#F6F5FF]'>
-                <div className="lg:w-9/12 w-11/12 mx-auto">
-                    <h2 className='text-[#101750] font-bold text-[36px] font-josefin'>Pages</h2>
-                    <div className="flex items-center lg:mt-2 mt-1">
-                        <p>Home.pages.</p><p className='text-[#FB2E86]'>Pages</p>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <section className='bg-[#F6F5FF] py-8 sm:py-10 lg:py-20'>
+      <div className="w-11/12 lg:w-9/12 mx-auto">
+        
+        <h2 className='text-[#101750] font-bold font-josefin 
+                       text-[24px] sm:text-[28px] lg:text-[36px]'>
+          Pages
+        </h2>
+
+        <div className="flex items-center flex-wrap gap-1 mt-1 lg:mt-2 text-[14px] sm:text-[15px] lg:text-[16px]">
+          <p>Home . Pages .</p>
+          <p className='text-[#FB2E86]'>Pages</p>
+        </div>
+
+      </div>
+    </section>
+  )
 }
 
 export default PageBanner
